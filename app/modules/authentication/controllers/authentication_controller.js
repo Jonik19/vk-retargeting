302e6fef22faff921255a47ef5ed160679271f4a
Danil <busymangorbenko@gmail.com>
2016-01-18 14:24:46 +0200

    use object instead class in authentication ctrl.

--- app/modules/authentication/controllers/authentication_controller.js
+++ app/modules/authentication/controllers/authentication_controller.js
@@ -6,52 +6,57 @@ var jwt = require('koa-jwt');
 var User = require('domains/user');
 
 /**
- * Class definition
- *
+ * Methods definition:
  */
 
-class AuthenticationController {
-  static get onlyAuthenticated() {
-    return jwt({ secret: config.secrets.authentication });
+var controller = {};
+
+controller.onlyAuthenticated = jwt({ secret: config.secrets.authentication });
+
+controller.onlyNotAuthenticated = function *(next) {
+  try {
+    jwt.verify(this.header.authentication, config.secrets.authentication);
+  } catch(err) {
+    yield next;
+  }
+};
+
+controller.signIn = function *(next) {
+  var data = {
+    username: this.request.body.username,
+    password: this.request.body.password
+  };
+
+  var user = yield User.checkCredentials(data);
+
+  if(user === null) {
+    throw {message: 'Incorrect data', status: 400};
   }
 
-  static *onlyNotAuthenticated(next) {
-    try {
-      jwt.verify(this.header.authentication, config.secrets.authentication);
-    } catch(err) {
-      yield next;
-    }
-  }
+  this.body = jwt.sign(user, config.secrets.authentication);
+};
 
-  static *signIn(next) {
-    var data = {
-      username: this.request.body.username,
-      password: this.request.body.password
-    };
+controller.signUp = function *(next) {
+  var data = {
+    username: this.request.body.username,
+    name: this.request.body.name,
+    password: this.request.body.password
+  };
 
-    var user = yield User.checkCredentials(data);
+  var user = yield User.create(data);
 
-    if(user === null) {
-      throw {message: 'Incorrect data', status: 400};
-    }
+  this.body = {
+    user: user,
+    token: jwt.sign(user, config.secrets.authentication)
+  };
+};
 
-    this.body = jwt.sign(user, config.secrets.authentication);
-  }
+/**
+ * Helper functions definition:
+ */
 
-  static *signUp(next) {
-    var data = {
-      username: this.request.body.username,
-      name: this.request.body.name,
-      password: this.request.body.password
-    };
-
-    var user = yield User.create(data);
-
-    this.body = {
-      user: user,
-      token: jwt.sign(user, config.secrets.authentication)
-    };
-  }
+function helper() {
+  // I do something a little
 }
 
-module.exports = AuthenticationController;
+module.exports = controller;
