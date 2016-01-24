a99f6985d7bdfacea8c7e853c499f8308c2b433d
Danil Gorbenko <busymangorbenko@gmail.ru>
2016-01-24 02:08:04 +0200

    added room repository

---
+++ app/repositories/room.js
@@ -0,0 +1,121 @@
+'use strict';
+
+var config = require('config');
+
+var Repository = require('helpers/repository');
+
+var UserDomain = require('domains/user');
+var RoomDomain = require('domains/room');
+
+var errors = require('modules/errors/services/errors');
+
+/**
+ * Model definition:
+ */
+
+var RoomRepo = function () {
+
+};
+
+/**
+ * Class methods definitions:
+ */
+
+/**
+ * Creates model and returns it.
+ */
+
+RoomRepo.create = function () {
+  return RoomDomain.create.apply(RoomDomain, arguments)
+    .then(function (user) {
+      return Repository.pickPublic(user, RoomDomain.publicFields);
+    });
+};
+
+/**
+ * Finds room by id.
+ */
+
+RoomRepo.findById = function () {
+  return RoomDomain.findById.apply(RoomDomain, arguments)
+    .then(function (user) {
+      return Repository.pickPublic(user, RoomDomain.publicFields);
+    });
+};
+
+/**
+ * Get room users.
+ */
+
+RoomRepo.getUsers = function (roomId) {
+  return RoomDomain.build({id: roomId}).getUsers({ attributes: UserDomain.publicFields})
+    .then(function (users) {
+      return users.map(function (user) {
+        return Repository.pickPublic(user, UserDomain.publicFields);
+      });
+    });
+};
+
+/**
+ * Enters to specified room. If user is already in room throws 'AlreadyInRoomError' error.
+ *
+ * @param {Object} options
+ * @param {String} options.room_id room_id to enter
+ * @param {String} options.user_id user_id to enter to room
+ * @returns {Promise.<T>|*}
+ */
+
+RoomRepo.enter = function (options) {
+  options = options || {};
+
+  return RoomDomain.findById(options.room_id)
+    .then(function (room) {
+      if(null === room) throw new errors.IncorrectDataError();
+
+      return RoomRepo.enterByModel({model: room, user_id: options.user_id});
+    });
+};
+
+/**
+ * Enters to specified room by existing model.
+ * If user is already in room throws 'AlreadyInRoomError' error.
+ *
+ * @param {Object} options
+ * @param {Object} options.model RoomDomain instance
+ * @param {String} options.user_id user_id to enter to room
+ * @returns {Promise.<T>|*}
+ */
+
+RoomRepo.enterByModel = function (options) {
+  options = options || {};
+
+  return options.model.addUser(options.user_id).then(function (inserted) {
+        // if nothing is added throw an error. It means that current user is already in this room
+        if(0 === inserted.length) throw new errors.AlreadyInRoomError();
+
+        return Repository.pickPublic(options.model, RoomDomain.publicFields);
+      });
+};
+
+/**
+ * Creates room and enters to this room by current user.
+ * If user is already in room throws 'AlreadyInRoomError' error.
+ *
+ * @param data ID of room to enter.
+ * @returns {Promise.<T>|*}
+ */
+
+RoomRepo.createAndEnter = function (data) {
+  data = data || {};
+
+  return RoomDomain.create.call(RoomDomain, data).
+    then(function (room) {
+      return RoomRepo.enterByModel({model: room, user_id: data.user_id});
+    });
+};
+
+/**
+ * Helper functions
+ */
+
+module.exports = RoomRepo;
