6de4d15bb28b64f26fb4a3f23cf0934f4d5b3cf3
Danil Gorbenko <busymangorbenko@gmail.ru>
2016-01-24 02:07:17 +0200

    added Repository helper

---
+++ app/helpers/repository.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var _ = require('lodash');
+var config = require('config');
+
+function Repository() {
+
+}
+
+/**
+ * Returns object with allowed fields which are passed through second parameter.
+ *
+ * @param {Object} model Domain instance
+ * @param {Array} publicFields Array of public fields
+ * @returns {Object} Object with
+ */
+
+Repository.pickPublic = function (model, publicFields) {
+  if(model === null) return model;
+
+  return _.pick(model, publicFields);
+};
+
+module.exports = Repository;
