75bb578a0dcd016592f1efb2157c456abd23c64e
Danil <busymangorbenko@gmail.com>
2016-01-27 18:35:00 +0200

    added create method in the purchases controller

--- app/modules/admin/purchases/controllers/purchases_controller.js
+++ app/modules/admin/purchases/controllers/purchases_controller.js
@@ -6,6 +6,8 @@ var Response = require('helpers/response');
 var errors = require('modules/errors/services/errors');
 
 var PurchaseRepo = require('repositories/purchase');
+var UserRepo = require('repositories/user');
+
 
 /**
  * Methods definition:
@@ -20,10 +22,41 @@ var controller = {};
  */
 
 controller.index = function *(next) {
+
+  // TODO: check user existing in db
+
   let purchases = yield PurchaseRepo.getPurchasesByRoomId(this.params.roomId);
 
   let response = new Response(this, purchases);
   response.items();
+};
+
+/**
+ * Creates purchase.
+ *
+ * @param next
+ */
+
+controller.create = function *(next) {
+  let data = {
+    owner_id: this.state.user.id, // id of user which makes this purchase
+    room_id: this.params.roomId, // id of the room to insert this purchase
+    name: this.request.body.name, // name of the purchase
+    amount: this.request.body.amount, // Full price of the purchase
+    // TODO: users must be an array
+    users: this.request.body.users // array of users ids
+  };
+
+  // Check user existing in this room
+  yield UserRepo.assertUserInRoom({
+    user_id: data.owner_id,
+    room_id: data.room_id
+  });
+
+  let purchase = yield PurchaseRepo.create(data);
+
+  let response = new Response(this, purchase);
+  response.success();
 };
 
 module.exports = controller;
